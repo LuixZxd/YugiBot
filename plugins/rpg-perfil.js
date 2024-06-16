b6dfd821f55383365c72fa15945df2c27037211d
Aldair-ld <frankaldair29@gmail.com>
2024-06-16 02:35:09 -0500

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -19,10 +19,7 @@ let username = conn.getName(who)
 let prem = global.prems.includes(who.split`@`[0])
 let sn = createHash('md5').update(who).digest('hex')
 let str =
-`HOLA BIENVENIDO
-
-*SU PERFIL <>*
-
+`
 ┃ *[ 👤 ] USUARIO:* ${name} ${user.registered === true ? 'ͧͧͧͦꙶͣͤ✓ᚲᴳᴮ' : ''}
 ┃ *[ ⚛️ ] NUMERO:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
 ┃ *[ ♠ ] ENLACE:* wa.me/${who.split`@`[0]}${registered ?'\n┃ 𝙀𝘿𝘼𝘿 ' + age + ' *años*' : ''}
