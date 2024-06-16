cbb14a3effd0320e2f1cc1d2aadde52d75cf2616
Aldair-ld <frankaldair29@gmail.com>
2024-06-15 22:58:23 -0500

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -8,7 +8,9 @@ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status
 let pp = gataImg.getRandom()	
 const cat = `
 ${global.fantasy} 𝒃𝒚 ${global.wm}
-𝙰 𝙲𝙾𝙽𝚃𝙸𝙽𝚄𝙰𝙲𝙸𝙾𝙽 𝚂𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙰 𝙴𝙻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙾 𝙳𝙴 𝙼𝙸𝚂 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾(𝙰)`
+𝙰 𝙲𝙾𝙽𝚃𝙸𝙽𝚄𝙰𝙲𝙸𝙾𝙽 𝚂𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙰 𝙴𝙻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙾 𝙳𝙴 LOS CREADORES DE 
+
+*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*`
 let biografiaBot = await conn.fetchStatus(conn.user.jid.split('@')[0] + '@s.whatsapp.net').catch(_ => 'undefined')
 let bioBot = biografiaBot.status?.toString() || `${desc2 == '' ? lenguajeGB.smsContacto1() : desc2}`
 
@@ -22,14 +24,14 @@ let biografia = await conn.fetchStatus(number +'@s.whatsapp.net').catch(_ => 'un
 let bio = biografia.status?.toString() || `${desc2 == '' ? lenguajeGB.smsContacto2() : desc2}`
   
 nombre = official[0][0] == String(contact[0]) ? official[0][1] : official[1][0] == String(contact[0]) ? official[1][1] : official[2][0] == String(contact[0]) ? official[2][1] : official[3][0] == String(contact[0]) ? official[3][1] : lenguajeGB.smsContacto3() 
-description = official[0][0] == String(contact[0]) ? 'FantasyBot-MD' : official[1][0] == String(contact[0]) ? 'FantasyBot-MD' : official[2][0] == String(contact[0]) ? 'FantasyBot-MD' : official[3][0] == String(contact[0]) ? 'FantasyBot-MD' : desc === '' ? 'FantasyBot-MD' : desc
-correo = official[0][0] == String(contact[0]) ? 'wilsmac68@gamil.com' : official[1][0] == String(contact[0]) ? 'wilsmac68@gmail.com' : official[2][0] == String(contact[0]) ? 'indefinido202@gmail.com' : mail === '' ? lenguajeGB.smsContacto6() : mail
+description = official[0][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : official[1][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : official[2][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : official[3][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : desc === '' ? 'FantasyBot-MD' : desc
+correo = official[0][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : official[1][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : official[2][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : mail === '' ? lenguajeGB.smsContacto6() : mail
 lugar = official[0][0] == String(contact[0]) ? '🇺🇸 Estados Unidos' : official[1][0] == String(contact[0]) ? '🇺🇸 Estados Unidos' : official[2][0] == String(contact[0]) ? '🇺🇲 Estados Unidos' : official[3][0] == String(contact[0]) ? '🇺🇸 Estados Unidos' : country === '' ? lenguajeGB.smsContacto7() : country
-enlace = official[0][0] == String(contact[0]) ? 'https://github.com/Wilsmac' : official[1][0] == String(contact[0]) ? 'https://github.com/Wilsmac' : official[2][0] == String(contact[0]) ? 'https://github.com/Wilsmac' : official[3][0] == String(contact[0]) ? 'https://github.com/Wilsmac' : md 
+enlace = official[0][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : official[1][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : official[2][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : official[3][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : md 
    
-lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? 'https://www.youtube.com/wilmer.oficial' : null]) }  
-lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), fantasy, lenguajeGB.smsContacto8(), mail === '' ? 'wilsmac68@gmail.com' : mail, lenguajeGB.smsContacto7(), md, bioBot, yt, ig, fb, paypal, nna])
-await conn.sendFile(m.chat, pp, 'lp.jpg',  cat, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 𝑾𝒊𝒍𝒎𝒆𝒓 𝒐𝒇𝒄 ', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb.getRandom()}}})
+lista.push([number, ofc, nombre, description, official[3][0] == String(contact[0]) ? null : correo, lugar, enlace, bio, official[1][0] == String(contact[0]) ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : null]) }  
+lista.push([conn.user.jid.split('@')[0], await conn.getName(conn.user.jid), fantasy, lenguajeGB.smsContacto8(), mail === '' ? '*_𝙶𝙾𝙺𝚄_𝙱𝙾𝚃 - 𝙼𝙳_*' : mail, lenguajeGB.smsContacto7(), md, bioBot, yt, ig, fb, paypal, nna])
+await conn.sendFile(m.chat, pp, 'lp.jpg',  cat, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' ALDAIR OFC ', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb.getRandom()}}})
 await conn.sendContactArray(m.chat, lista, null, { quoted: fkontak })
   
 } catch (e) {
